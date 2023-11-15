dd3fc2e6a218a97bdf671742ff8c2e4523105fcf
SEA-365 <whm360735@163.com>
2023-11-15 21:40:38 +0800

    :smile:20231115-解决Shiro框架和Spring Security框架之间对Bcrypt算法实现的不一致问题

---
+++ src/main/java/com/sea/ResourcesConfig.java
@@ -0,0 +1,5 @@
+package com.sea;/**
+ * @author: sea
+ * @date: 2023/11/15 17:30
+ */public class ResourcesConfig {
+}
---
+++ src/main/java/com/sea/shiro/SpringSecurityBCryptMatcher.java
@@ -0,0 +1,5 @@
+package com.sea.shiro;/**
+ * @author: sea
+ * @date: 2023/11/15 21:10
+ */public class SpringSecurityBCryptMatcher {
+}
